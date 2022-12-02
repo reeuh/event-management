e88d29972fba08c2d6342fba71c3f38ce6a02605
Faye Lacsi <its_faye_lacsi@yahoo.com>
2022-12-02 21:32:00 +0800

    Update eventmanagementsystem.sql
    update managemenr system

--- eventmanagementsystem.sql
+++ eventmanagementsystem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2022 at 11:12 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Dec 02, 2022 at 02:30 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,21 +30,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `account` (
   `user_id` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
+  `midname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account`
 --
 
-INSERT INTO `account` (`user_id`, `firstname`, `lastname`, `type`, `username`, `password`) VALUES
-(1, 'Roselyn', 'Tarroza', 'admin', 'roselyn_tarroza', 'thepretty'),
-(2, 'Ma. Recca Faye', 'Lacsi', 'admin', 'faye', 'faye'),
-(3, 'Jasrha', 'Musa', 'admin', 'jasrha', 'jasrha'),
-(4, 'Gian', 'Garcia', 'admin', 'gian', 'gian');
+INSERT INTO `account` (`user_id`, `firstname`, `midname`, `lastname`, `type`, `username`, `password`) VALUES
+(1, 'Roselyn', '', 'Tarroza', 'admin', 'roselyn_tarroza', 'thepretty'),
+(2, 'Ma. Recca Faye', '', 'Lacsi', 'admin', 'faye', 'faye'),
+(3, 'Jasrha', '', 'Musa', 'client', 'jasrha', 'jasrha'),
+(4, 'Gian', '', 'Garcia', 'admin', 'gian', 'gian');
 
 -- --------------------------------------------------------
 
@@ -59,21 +60,22 @@ CREATE TABLE `event` (
   `phone_no` varchar(11) NOT NULL,
   `event` varchar(255) NOT NULL,
   `schedule` datetime NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '1-Public\r\n2-Private',
+  `type` varchar(10) NOT NULL COMMENT '1-Public\r\n2-Private',
   `audience_capacity` int(11) NOT NULL,
-  `payment_type` tinyint(4) NOT NULL COMMENT '1-Free\r\n2-Payable',
+  `payment_type` varchar(10) NOT NULL COMMENT '1-Free\r\n2-Payable',
   `amount` double NOT NULL,
   `status` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event`
 --
 
 INSERT INTO `event` (`id`, `client`, `address`, `phone_no`, `event`, `schedule`, `type`, `audience_capacity`, `payment_type`, `amount`, `status`, `created_at`, `updated_at`) VALUES
-(2, 'Rose', 'Lunzuran', '09563784736', 'Birthday', '2022-11-09 07:00:00', 1, 234, 2, 2345, '', '2022-11-27 12:40:05', '2022-11-27 12:40:05');
+(4, 'Rose', 'Bukid', '097843743', 'Birthdays', '2022-12-21 16:21:00', 'public', 245, 'payable', 757995, 'Active', '2022-12-02 08:21:49', '2022-12-02 08:21:49'),
+(6, 'hsjkksndn', 'hssjjsj', '23467654', 'Birthdays', '2022-12-23 21:35:00', 'ad', 345654, 'sdfghhgfd', 23456, 'Active', '2022-12-02 09:36:15', '2022-12-02 09:36:15');
 
 -- --------------------------------------------------------
 
@@ -87,7 +89,7 @@ CREATE TABLE `event category` (
   `Created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `Updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `Action` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event category`
@@ -98,6 +100,19 @@ INSERT INTO `event category` (`ID`, `Classification`, `Created_at`, `Updated_at`
 ('#222', 'Wedding', '2022-11-22 12:51:58', '2022-11-22 20:51:58', ''),
 ('#333', 'Anniversary', '2022-11-22 12:52:51', '2022-11-22 20:52:51', ''),
 ('#444', 'Christening', '2022-11-22 12:53:11', '2022-11-22 20:53:11', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue`
+--
+
+CREATE TABLE `venue` (
+  `venue` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `rate` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -137,7 +152,7 @@ ALTER TABLE `account`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
